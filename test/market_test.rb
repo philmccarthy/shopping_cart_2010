c5791f0880535d800d77eb3f2666dce166be1ddc
Phil McCarthy <philmcc2631@gmail.com>
2020-11-11 09:21:06 -0700

    Add test for vendors_that_sell(item) method

--- test/market_test.rb
+++ test/market_test.rb
@@ -33,20 +33,25 @@ class MarketTest < Minitest::Test
     market.add_vendor(vendor3)
     assert_equal ["Rocky Mountain Fresh", "Ba-Nom-a-Nom", "Palisade Peach Shack"], market.vendor_names
   end
-end
 
-#
-# item1 = Item.new({name: 'Peach', price: "$0.75"})
-# item2 = Item.new({name: 'Tomato', price: "$0.50"})
-# item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
-# item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
-#
-# vendor1.stock(item1, 35)
-#
-# vendor1.stock(item2, 7)
-#
-# vendor2.stock(item4, 50)
-#
-# vendor2.stock(item3, 25)
-#
-# vendor3.stock(item1, 65)
+  def test_it_can_find_all_vendors_that_sell_an_item
+    market = Market.new("South Pearl Street Farmers Market")
+    vendor1 = Vendor.new("Rocky Mountain Fresh")
+    vendor2 = Vendor.new("Ba-Nom-a-Nom")
+    vendor3 = Vendor.new("Palisade Peach Shack")
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+    item2 = Item.new({name: 'Tomato', price: "$0.50"})
+    item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+    vendor1.stock(item1, 35)
+    vendor1.stock(item2, 7)
+    vendor2.stock(item4, 50)
+    vendor2.stock(item3, 25)
+    vendor3.stock(item1, 65)
+    market.add_vendor(vendor1)
+    market.add_vendor(vendor2)
+    market.add_vendor(vendor3)
+    assert_equal [vendor1, vendor3], market.vendors_that_sell(item1)
+    assert_equal [vendor2], market.vendors_that_sell(item4)
+  end
+end
