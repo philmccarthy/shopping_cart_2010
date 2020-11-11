b0d91720b406b0c2f4b33f0772918950f93cdb67
Phil McCarthy <philmcc2631@gmail.com>
2020-11-11 10:27:42 -0700

    Add test for item_inventory lookup method

--- test/market_test.rb
+++ test/market_test.rb
@@ -216,4 +216,29 @@ class MarketTest < Minitest::Test
     assert_equal 0, vendor1.check_stock(item1)
     assert_equal 60, vendor3.check_stock(item1)
   end
+
+  def test_it_can_look_up_current_inventory_of_single_item
+    market = Market.new("South Pearl Street Farmers Market")
+    vendor1 = Vendor.new("Rocky Mountain Fresh")
+    vendor2 = Vendor.new("Ba-Nom-a-Nom")
+    vendor3 = Vendor.new("Palisade Peach Shack")
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+    item2 = Item.new({name: 'Tomato', price: "$0.50"})
+    item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+    vendor1.stock(item1, 35)
+    vendor1.stock(item2, 7)
+    vendor2.stock(item4, 50)
+    vendor2.stock(item3, 25)
+    vendor3.stock(item1, 65)
+    market.add_vendor(vendor1)
+    market.add_vendor(vendor2)
+    market.add_vendor(vendor3)
+    assert_equal 100, market.item_inventory(item1)
+    assert_equal 7, market.item_inventory(item2)
+    assert_equal 25, market.item_inventory(item3)
+    assert_equal 50, market.item_inventory(item4)
+    vendor1.stock(item1, -25)
+    assert_equal 75, market.item_inventory(item1)
+  end
 end
