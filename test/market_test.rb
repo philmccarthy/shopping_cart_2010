1d28f4f64f8f4376bfc37904f002a4eec0332310
Phil McCarthy <philmcc2631@gmail.com>
2020-11-11 10:15:39 -0700

    Add trickle_sell helper method

--- test/market_test.rb
+++ test/market_test.rb
@@ -191,4 +191,29 @@ class MarketTest < Minitest::Test
     assert_equal true, market.unsellable?(item5, 1)
     assert_equal false, market.unsellable?(item4, 5)
   end
+
+  def test_it_can_trickle_sell_from_vendors
+    market = Market.new("South Pearl Street Farmers Market")
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+    item2 = Item.new({name: 'Tomato', price: '$0.50'})
+    item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+    item5 = Item.new({name: 'Onion', price: '$0.25'})
+    vendor1 = Vendor.new("Rocky Mountain Fresh")
+    vendor1.stock(item1, 35)
+    vendor1.stock(item2, 7)
+    vendor2 = Vendor.new("Ba-Nom-a-Nom")
+    vendor2.stock(item4, 50)
+    vendor2.stock(item3, 25)
+    vendor3 = Vendor.new("Palisade Peach Shack")
+    vendor3.stock(item1, 65)
+    market.add_vendor(vendor1)
+    market.add_vendor(vendor2)
+    market.add_vendor(vendor3)
+    market.trickle_sell(item4, 5)
+    assert_equal 45, vendor2.check_stock(item4)
+    market.trickle_sell(item1, 40)
+    assert_equal 0, vendor1.check_stock(item1)
+    assert_equal 60, vendor3.check_stock(item1)
+  end
 end
