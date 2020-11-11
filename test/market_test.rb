43c2c7960f904982fd2a3bb9ab4523b4825a9393
Phil McCarthy <philmcc2631@gmail.com>
2020-11-11 10:36:00 -0700

    Add test for overstocked?(item) method

--- test/market_test.rb
+++ test/market_test.rb
@@ -241,4 +241,28 @@ class MarketTest < Minitest::Test
     vendor1.stock(item1, -25)
     assert_equal 75, market.item_inventory(item1)
   end
+
+  def test_it_can_check_if_an_item_is_overstocked
+    market = Market.new("South Pearl Street Farmers Market")
+    item1 = Item.new({name: "Peach", price: "$0.75"})
+    item2 = Item.new({name: "Tomato", price: "$0.50"})
+    item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+    vendor1 = Vendor.new("Rocky Mountain Fresh")
+    vendor1.stock(item1, 35)
+    vendor1.stock(item2, 7)
+    vendor2 = Vendor.new("Ba-Nom-a-Nom")
+    vendor2.stock(item4, 50)
+    vendor2.stock(item3, 25)
+    vendor3 = Vendor.new("Palisade Peach Shack")
+    vendor3.stock(item1, 65)
+    vendor3.stock(item3, 10)
+    market.add_vendor(vendor1)
+    market.add_vendor(vendor2)
+    market.add_vendor(vendor3)
+    assert_equal true, market.overstocked?(item1)
+    assert_equal false, market.overstocked?(item3)
+    vendor3.stock(item3, 100)
+    assert_equal true, market.overstocked?(item3)
+  end
 end
